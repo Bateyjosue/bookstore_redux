85d5d11cd5b5e03c8c046f5523cd375826e026d7
Bateyjosue <josuebatey19@gmail.com>
2022-09-16 09:17:08 +0200

    Create InputBook Component

---
+++ src/components/InputBook.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function InputBook() {
+  return (
+    <form className='form-add__book'>
+      <input type="text" name="title" id="title" placeholder="Book Title" />
+      <input type="text" name="category" id="category" placeholder="Book Author" />
+      <input type="submit" name="submit" id="submit"  value="Add Book" />
+    </form>
+  )
+}
