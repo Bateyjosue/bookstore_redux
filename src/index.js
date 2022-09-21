2c08b6a8c32a8da3c65a4509450689b458fa0147
Bateyjosue <josuebatey19@gmail.com>
2022-09-21 15:26:01 -0700

    Add redux Provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-
+import store from './redux/configureStore';
+import { Provider } from 'react-redux';
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </>,
+  </Provider>,
   document.getElementById('root'),
 );
