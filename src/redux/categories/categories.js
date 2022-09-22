8e9e816b16577239b0cb39629cc18d68ea71ae6c
Bateyjosue <josuebatey19@gmail.com>
2022-09-22 13:18:54 -0700

    Edit Category Action to return object a text on it

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,8 +1,8 @@
 const CHECK_STATUS = 'CHECK_STATUS';
 const initialState = [];
 
-const reducerCategory = (state = initialState, action) => (action.type === CHECK_STATUS ? { text: 'Under Construction' } : state);
+const reducerCategory = (state = initialState, action) => (action.type === CHECK_STATUS ? action.text : state);
 
-export const checkStatus = () => ({ type: CHECK_STATUS });
+export const checkStatus = () => ({ type: CHECK_STATUS, text: 'Under Construction' });
 
 export default reducerCategory;
