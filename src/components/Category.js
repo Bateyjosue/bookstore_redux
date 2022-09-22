42e03b6e4fbc93e4b208968aabb2c396bc15e142
Bateyjosue <josuebatey19@gmail.com>
2022-09-22 13:20:03 -0700

    Dispatch the state of category on button click

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {checkStatus} from '../redux/categories/categories';
 
 export default function Category() {
+  const status = useSelector((state) => state.category );
+  
+  const dispatch = useDispatch();
+
+  const handleCheckStatus = () => {
+    dispatch(checkStatus());
+    console.log(`Category: ${status}`);
+  }
   return (
     <div>
-      <h1>The Category is Under Constrution</h1>
+      <button type="button" id="submit" onClick={handleCheckStatus}>Add Book</button>
+      <h1>{status}</h1>
     </div>
   );
 }
