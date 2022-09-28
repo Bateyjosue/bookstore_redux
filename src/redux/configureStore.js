aa05748debd781d2ccb1b981905ccbcaee8707ee
Bateyjosue <josuebatey19@gmail.com>
2022-09-28 17:00:38 +0200

    Add the thunk middleware to the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,25 +1,14 @@
 import { combineReducers, createStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import reducerCategory from './categories/categories';
-// import { uid } from 'uid';
-// const initialState = [
-//   {
-//     id: uid(10),
-//     title: 'Book One',
-//     author: 'Josh Batey'
-//   },
-//   {
-//     id: uid(10),
-//     title: 'Book Two',
-//     author: 'Joe Baen'
-//   },
-// ];
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   books: bookReducer,
   category: reducerCategory,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
